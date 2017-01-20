ed40b1eeae4037091095356013c7d5df608ef836
rathomas17 <rathomas17@gmail.com>
2017-01-20 12:02:30 -0500

    added row for pics

--- js/index.js
+++ js/index.js
@@ -151,6 +151,46 @@ img.onclick = function(){
     captionText.innerHTML = this.alt;
 };
 
+var img = document.getElementById('myImg12');
+
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+};
+
+var img = document.getElementById('myImg9');
+
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+};
+
+var img = document.getElementById('myImg10');
+
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+};
+
+var img = document.getElementById('myImg11');
+
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+};
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
